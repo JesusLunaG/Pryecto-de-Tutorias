380556e53ab119b582e0bb72d262bac54647f222
JesusLunaG <72886356+JesusLunaG@users.noreply.github.com>
2022-05-19 23:40:49 -0500

    add: string connection to azure

--- Titulacion/Models/TutoriasContext.cs
+++ Titulacion/Models/TutoriasContext.cs
@@ -29,7 +29,7 @@ namespace Titulacion.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Data Source=Asura;Initial Catalog=Tutorias;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Server=tcp:tutoriasexpress.database.windows.net,1433;Initial Catalog=TutoriasExpress;Persist Security Info=False;User ID=administrador;Password=Tutorias1.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
