b8832d81a9dc543bf3b50d3a1f4f6e22cc63246f
Luis Angel Barajas <99161300+BarajasAngel@users.noreply.github.com>
2022-06-09 18:09:53 -0500

    Update TutoriasContext.cs

--- Titulacion/Models/TutoriasContext.cs
+++ Titulacion/Models/TutoriasContext.cs
@@ -29,7 +29,7 @@ namespace Titulacion.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=tcp:tutoriaexpress.database.windows.net,1433;Initial Catalog=tutoriaexpress;Persist Security Info=False;User ID=administrador;Password=adminadmin1.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=tcp:tutoriaexpress.database.windows.net,1433;Initial Catalog=tutoriaexpress;Persist Security Info=False;User ID=Administrador;Password=Tutoria1.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
